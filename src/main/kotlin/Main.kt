3881a84036cb2286e8f25ecaeed44f3351fb8502
Kristian Brudeli <krbrudeli@gmail.com>
2025-07-16 15:11:53 +0200

    Add Token data class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -55,6 +55,15 @@ enum class TokenType {
     EOF;
 }
 
+data class Token(
+    val type: TokenType,
+    val lexeme: String,
+    val literal: Any,
+    val line: Int
+) {
+    override fun toString(): String = "$type $lexeme $literal"
+}
+
 class Lox {
     var hadError = false
 
