177e235fb385358fece0e2d27670e42e2ef3ae03
Kristian Brudeli <krbrudeli@gmail.com>
2025-07-16 14:52:03 +0200

    Implement runPrompt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -14,7 +16,14 @@ class Lox {
     }
 
     fun runPrompt() {
-        TODO("runPrompt not implemented!")
+        val input = InputStreamReader(System.`in`)
+        val reader = BufferedReader(input)
+
+        while (true) {
+            print("> ")
+            val line = reader.readLine() ?: break
+            run(line)
+        }
     }
 
     fun main(args: Array<String>) {
