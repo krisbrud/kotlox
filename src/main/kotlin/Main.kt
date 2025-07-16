c464de4b22559fc05c8cd83f032a5cc07ab80984
Kristian Brudeli <krbrudeli@gmail.com>
2025-07-16 16:14:07 +0200

    Support two-char lexemes in scanner

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -82,6 +82,53 @@ data class Scanner(
         tokens.add(Token(TokenType.EOF, "", null, line))
     }
 
+    fun advance(): Char {
+        return source.get(current++)
+    }
+
+    fun addToken(type: TokenType) {
+        addToken()
+    }
+
+    fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+
+    fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+        current++
+        return true
+    }
+
+    fun scanToken() {
+        val c = advance()
+
+        when (c) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+
+            '!' -> addToken(if (match('=')) TokenType.BANG_EQUAL else TokenType.BANG)
+            '=' -> addToken(if (match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL)
+            '<' -> addToken(if (match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
+            '>' -> addToken(if (match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
+
+            else -> {
+                error("line: $line, unexpected character.")
+            }
+        }
+    }
+
     fun isAtEnd(): Boolean = current >= source.length
 }
 
