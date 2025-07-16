e4a58d46378adf19ba60ba04c945b86d347274f4
Kristian Brudeli <krbrudeli@gmail.com>
2025-07-16 15:05:50 +0200

    More work on skeleton and create tokentype enum class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,14 +5,83 @@ import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
 
+enum class TokenType {
+    // Single char tokens
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two char tokens
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF;
+}
+
 class Lox {
+    var hadError = false
+
     fun run(source: String) {
-        TODO("run not implemented")
+        val scanner = Scanner(source)
+        val tokens: List<Token> = scanner.scanTokens()
+
+        // Just print the tokens for now
+        for (token in tokens) {
+            print(token)
+        }
+    }
+
+    fun error(line: Int, message: String) {
+        report(line, "", message)
+        hadError = true
+    }
+
+    fun report(line: Int, where: String, message: String) {
+        println("[line $line] Error $where : $message")
     }
 
     fun runFile(path: String) {
         val source = File(path).readText(charset = Charset.defaultCharset())
         run(source)
+
+        if (hadError) System.exit(65)
     }
 
     fun runPrompt() {
@@ -23,6 +92,7 @@ class Lox {
             print("> ")
             val line = reader.readLine() ?: break
             run(line)
+            hadError = false
         }
     }
 
