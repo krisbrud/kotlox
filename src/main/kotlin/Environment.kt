037d447e3d1bbe39f5cc0ece129d40912643e51b
Kristian Brudeli <krbrudeli@gmail.com>
2025-09-20 13:30:38 +0200

    Support enclosing scopes

--- src/main/kotlin/Environment.kt
+++ src/main/kotlin/Environment.kt
@@ -1,8 +1,11 @@
 class Environment(
-    private val values: MutableMap<String, Any?> = mutableMapOf()
+    val enclosing: Environment? = null,
 ) {
+    private val values: MutableMap<String, Any?> = mutableMapOf()
     fun get(name: Token): Any? {
         return values.getOrElse(name.lexeme) {
+            if (enclosing != null) return enclosing.get(name)
+
             throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
         }
     }
@@ -14,6 +17,8 @@ class Environment(
     fun assign(name: Token, value: Any?) {
         if (values.containsKey(name.lexeme)) {
             values[name.lexeme] = value
+        } else if (enclosing != null) {
+            enclosing.assign(name, value)
         } else {
             throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
         }
