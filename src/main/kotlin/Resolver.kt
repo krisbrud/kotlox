d4d4803f6a27030f3437a2678094c33b80cdaf84
Kristian Brudeli <krbrudeli@gmail.com>
2025-09-21 21:06:15 +0200

    Implement visitor interface for resolver

---
+++ src/main/kotlin/Resolver.kt
@@ -0,0 +1,144 @@
+import java.sql.Statement
+import java.util.Stack
+
+class Resolver(private val interpreter: Interpreter, private val errorReporter: (Token, String) -> Unit) :
+    Expr.Visitor<Unit>, Stmt.Visitor<Unit> {
+    val scopes: Stack<MutableMap<String, Boolean>> = Stack()
+
+    fun resolve(statements: List<Stmt>) {
+        statements.forEach { resolve(it) }
+    }
+
+    private fun resolve(stmt: Stmt) {
+        stmt.accept(this)
+    }
+
+    private fun resolve(expr: Expr) {
+        expr.accept(this)
+    }
+
+    private fun beginScope() {
+        scopes.push(mutableMapOf())
+    }
+
+    private fun endScope() {
+        scopes.pop()
+    }
+
+    private fun declare(name: Token) {
+        if (scopes.isEmpty()) return // Global scope?
+
+        val scope = scopes.peek()
+        scope[name.lexeme] = false
+    }
+
+    private fun define(name: Token) {
+        if (scopes.isEmpty()) return
+        scopes.peek()[name.lexeme] = true
+    }
+
+    private fun resolveLocal(expr: Expr, name: Token) {
+        for (i in (scopes.size - 1) downTo 0) {
+            if (scopes[i].containsKey(name.lexeme)) {
+                interpreter.resolve(expr, scopes.size - 1 - i)
+            }
+        }
+    }
+
+    fun resolveFunction(function: Stmt.Function) {
+        beginScope()
+        function.params.forEach { param ->
+            declare(param)
+            define(param)
+        }
+        resolve(function.body)
+        endScope()
+    }
+
+    override fun visitAssignExpr(expr: Expr.Assign) {
+        resolve(expr.value)
+        resolveLocal(expr, expr.name)
+    }
+
+    override fun visitBinaryExpr(expr: Expr.Binary) {
+        resolve(expr.left)
+        resolve(expr.right)
+    }
+
+    override fun visitCallExpr(expr: Expr.Call) {
+        resolve(expr.callee)
+
+        expr.arguments.forEach { arg -> resolve(arg) }
+    }
+
+    override fun visitGroupingExpr(expr: Expr.Grouping) {
+        resolve(expr.expression)
+    }
+
+    override fun visitLiteralExpr(expr: Expr.Literal) {
+        // Do nothing
+    }
+
+
+    override fun visitLogicalExpr(expr: Expr.Logical) {
+        resolve(expr.left)
+    }
+
+    override fun visitUnaryExpr(expr: Expr.Unary) {
+        resolve(expr.right)
+    }
+
+    override fun visitVariableExpr(expr: Expr.Variable) {
+        if (!scopes.isEmpty() && scopes.peek()[expr.name.lexeme] == false) {
+            errorReporter(expr.name, "Can't read local variable in its own initializer.")
+        }
+
+        resolveLocal(expr, expr.name)
+    }
+
+    override fun visitBlockStmt(stmt: Stmt.Block) {
+        beginScope()
+        resolve(stmt.statements)
+        endScope()
+    }
+
+    override fun visitExpressionStmt(stmt: Stmt.Expression) {
+        resolve(stmt.expression)
+    }
+
+    override fun visitFunctionStmt(stmt: Stmt.Function) {
+        declare(stmt.name)
+        define(stmt.name)
+
+        resolveFunction(stmt)
+    }
+
+    override fun visitIfStmt(stmt: Stmt.If) {
+        resolve(stmt.condition)
+        resolve(stmt.thenBranch)
+        stmt.elseBranch?.also { resolve(it) }
+    }
+
+    override fun visitPrintStmt(stmt: Stmt.Print) {
+        resolve(stmt.expression)
+    }
+
+    override fun visitReturnStmt(stmt: Stmt.Return) {
+        stmt.value?.also { resolve(it) }
+    }
+
+    override fun visitVarStmt(stmt: Stmt.Var) {
+        declare(stmt.name)
+        if (stmt.initializer != null) {
+            resolve(stmt.initializer)
+        }
+        define(stmt.name)
+    }
+
+    override fun visitWhileStmt(stmt: Stmt.While) {
+        resolve(stmt.condition)
+        resolve(stmt.body)
+    }
+
+
+}
